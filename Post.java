c74b146c67eef8e3b268ec7f57ff65da7539e180
Shamus E <shamusend@gmail.com>
2020-02-12 17:23:59 -0500

    removed public/private functionality

--- Post.java
+++ Post.java
@@ -2,15 +2,10 @@ public class Post{
     private User poster;
     private String text;
     private String date;
-    private boolean privacy;
     private String pstring;
     public Post(User p, String t, String pv, String d){
         poster = p;
         text = t;
-        if (pv.toLowerCase() == "public")
-            privacy = false;
-        else
-            privacy = true;
         date = d;    
         pstring = pv;
     }
